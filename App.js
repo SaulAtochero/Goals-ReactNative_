ead5e98f7970c8a17b86ba7fc9c4b6bedba5a281
SaulAtochero <saul5sanatonio@gmail.com>
2024-01-16 10:07:04 +0100

    Agregado Hook de Estado

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StyleSheet, TextInput, View, Button, Text } from 'react-native';
-
+import { StyleSheet, TextInput, View, Button, Text, LogBox } from 'react-native';
+import { useState } from 'react';
 export default function App() {
+
+  //Declaramos el Hook de estado de componente "newGoal"
+  //      "get"      "set"
+  //        |          |
+  //        V          V
+  const [newGoal, setNewGoal] = useState("");
+
+
+  function textChangeHandler(texto) {
+    setNewGoal(texto);
+    console.log(newGoal);
+  }
+
   return (
     <View style={style.container}>
       <View style={style.inputcontainer}>
-        <TextInput style={style.TextInput} placeholder='Introduce una meta' />
-        <Button title='Enviar' />
+        <TextInput
+          style={style.TextInput}
+          placeholder='Introduce una meta'
+          onChangeText={textChangeHandler}
+        />
+        <Button
+          title='Enviar'
+        />
       </View>
       <View style={style.goalsContainer}>
         <Text style={style.texto}> Tu lista de metas...</Text>
@@ -30,17 +49,24 @@ const style = new StyleSheet.create({
     borderBottomColor: "#2296f3",
     alignItems: "center",
   },
+  //-------------------------------------------------------------------------------------------//
   TextInput: {
     borderColor: "#2296f3",
     borderWidth: 1,
     width: "70%",
     padding: 8,
+    color: "#0c3659",
+    fontWeight: "bold",
   },
+  //-------------------------------------------------------------------------------------------//
   goalsContainer: {
     flex: 5,
   },
+  //-------------------------------------------------------------------------------------------//
   texto: {
     fontWeight: "bold",
     fontSize: 20,
   },
+  //-------------------------------------------------------------------------------------------//
+
 })
