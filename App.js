3acde3c619060d5398f78c98c98102fcd49feb0e
SaulAtochero <saul5sanatonio@gmail.com>
2024-01-16 09:27:02 +0100

    maqueta

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, Button, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.container}>
+      <View style={style.inputcontainer}>
+        <TextInput style={style.TextInput} placeholder='Introduce una meta' />
+        <Button title='Enviar' />
+      </View>
+      <View style={style.goalsContainer}>
+        <Text style={style.texto}> Tu lista de metas...</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
+const style = new StyleSheet.create({
+  //-------------------------------------------------------------------------------------------//
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
+    paddingHorizontal: 20,
   },
-});
+  //-------------------------------------------------------------------------------------------//
+  inputcontainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    borderBottomWidth: 2,
+    borderBottomColor: "#2296f3",
+    alignItems: "center",
+  },
+  TextInput: {
+    borderColor: "#2296f3",
+    borderWidth: 1,
+    width: "70%",
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  texto: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+})
