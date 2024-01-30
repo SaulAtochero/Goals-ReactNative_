46643aa3389c78abca0dcf3cca5c4d9fad1a63fa
SaulAtochero <saul5sanatonio@gmail.com>
2024-01-30 09:15:34 +0100

    ModalV2

--- componentes/MeterMeta.js
+++ componentes/MeterMeta.js
@@ -34,14 +34,23 @@ export default function MeterMeta({ nuevaMeta, visible, onCancel }) {
                     onChangeText={textChangeHandler}
 
                 />
-                <Button
-                    title='Cancelar'
-                    onPress={onCancel}
-                />
-                <Button
-                    title='Enviar'
-                    onPress={pulsar}
-                />
+                <View style={style.containerAcceptCancel}>
+                    <View style={style.boton}>
+                        <Button
+                            title='Cancelar'
+                            onPress={onCancel}
+                        />
+                    </View>
+
+                    <View style={style.boton}>
+                        <Button
+                            title='Enviar'
+                            onPress={pulsar}
+                        />
+                    </View>
+
+
+                </View>
             </View>
         </Modal>
     )
@@ -51,12 +60,37 @@ const style = new StyleSheet.create({
     //-------------------------------------------------------------------------------------------//
     inputcontainer: {
         flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-between",
+        flexDirection: "column",
+        justifyContent: "center",
         marginBottom: 20,
         borderBottomWidth: 2,
         borderBottomColor: "#2296f3",
         alignItems: "center",
+    },
+    //-------------------------------------------------------------------------------------------//
+    containerAcceptCancel: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 20,
+        alignItems: "center",
+    },
+    //-------------------------------------------------------------------------------------------//
+    boton: {
+        width: "30%",
+        marginTop: 10,
+        marginRight: 5,
+        marginLeft: 5,
+        borderWidth: 2,
+        borderColor: "#f13f42",
+    },
+    //-------------------------------------------------------------------------------------------//
+    TextInput: {
+        borderColor: "#2296f3",
+        borderWidth: 1,
+        width: "70%",
+        padding: 8,
+        color: "#0c3659",
+        fontWeight: "bold",
     },
     //-------------------------------------------------------------------------------------------//
 })
